d3bb66561f7ca8587091e72982f64e3b57480353
Oscar Villagran <oscar.villagran@hotmail.com>
2023-05-09 11:02:04 -0500

    Add logic

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import styles from "@/styles/Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1>To do list</h1>
+    </header>
+  );
+};
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TodoApp from '@/components/TodoApp';
+import '@/styles/app.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const domContainer = document.getElementById("root");
+const root = ReactDOM.createRoot(domContainer);
+
+root.render(
   <React.StrictMode>
-    <App />
+    <TodoApp />
   </React.StrictMode>,
-)
+);
+
